7b41f3ed6191c7ade8bace990ff0c476b5bf0fac
Joe Noon <joenoon@gmail.com>
2018-01-29 01:33:47 -0800

    wip npr auth step 1

--- expo/App.js
+++ expo/App.js
@@ -1,11 +1,35 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import { AuthSession } from 'expo';
+import {observer} from "mobx-react/native"
+import {observable} from 'mobx';
 
+// @todo: extract to a config
+const NPR_CLIENT_ID = 'nprone_trial_vNlQ3IUlacqy';
+
+@observer
 export default class App extends React.Component {
+
+  @observable result = null;
+
+  onLoginPress = async () => {
+    const redirectUrl = AuthSession.getRedirectUrl();
+    const result = await AuthSession.startAsync({
+      authUrl: `https://api.npr.org/authorization/v2/authorize?client_id=${NPR_CLIENT_ID}&redirect_uri=${encodeURIComponent(redirectUrl)}&response_type=code&scope=identity.readonly%20identity.write%20listening.readonly%20listening.write%20localactivation&state=CSRFTOKEN`
+    });
+    console.log('result', result);
+    this.result = result;
+  };
+
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        <TouchableOpacity onPress={this.onLoginPress}>
+          <View style={{backgroundColor:'#fff', borderRadius: 8,paddingHorizontal:40,paddingVertical:20}}>
+            <Text style={{color: '#4285f4', fontSize: 20}}>Log in to NPR</Text>
+          </View>
+        </TouchableOpacity>
+        <Text style={{color:'#fff'}}>{this.result ? 'yes' : 'none'}</Text>
       </View>
     );
   }
@@ -14,7 +38,7 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#000',
     alignItems: 'center',
     justifyContent: 'center',
   },
